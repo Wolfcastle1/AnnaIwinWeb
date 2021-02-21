796a1e37dced1bd1d118c173784f6e2fcd47e529
Samuel Thomas <54728781+Wolfcastle1@users.noreply.github.com>
2021-02-21 14:13:04 -0600

    Data Separation
    Separated Art piece data from the portfolio code to simplify the portfolio.js file

---
+++ anna-iwin-front/src/data/artInfo.js
@@ -0,0 +1,57 @@
+//Pieces
+import classicStatue from '../art/classicStatue.jpg';
+import spaceFace from '../art/spaceFace.jpg';
+import speederman from '../art/speederman.jpeg';
+import hannahElf from '../art/hannahElf.jpeg';
+import GoodnightMoon from '../art/Goodnight_Moon.jpg';
+import Citrus from '../art/Cictrus.jpg';
+import FairyForest from '../art/Fairy_Forest.jpg';
+import MushroomDate from '../art/Mushroom_date.jpg';
+import GreenSweater from  '../art/Green_sweater.jpg';
+import FantasyCL from '../art/Fantasy_Character_lineup.jpg';
+import FantasyScene from '../art/Fantasy_scene.jpg';
+import IsoTree from '../art/Isometric_treehouse.jpg'; 
+import Queen from '../art/Queen.jpg'; 
+// import WitchPatt from '../art/Witchy_pattern.jpg'; 
+import DeerGirl from '../art/Deer_Girl.jpeg'; 
+import TigerBomb from '../art/Tiger_Bomb.jpeg'; 
+import LastUnicorn from '../art/The_Last_Unicorn_cover.jpg'; 
+import Bloom from '../art/Bloom.jpg'; 
+import WitchesRoom from '../art/Witches_room.jpg'; 
+import CoolCat from '../art/Cool_Cat.jpg'; 
+// import PetComm from '../art/Pet_commission.jpg';
+import BreakfastPatt from '../art/Breakfast_Pattern.jpg';
+import GhostExp from '../art/Canterville_ghost_expression_sheet.jpg';
+import GhostPose from '../art/Canterville_ghost_hero_pose-and_turnarounds.jpg';
+import CloudPattern from '../art/Cloud_Pattern.jpg';
+import ForestPattern from '../art/Forest_Pattern.jpg';
+import homesick from '../art/homesick.jpg';
+
+export const pieces = [
+    { name: "Goodnight Moon", source: GoodnightMoon, medium: "Digital, Clip Studio Paint", desc: "Final assignment for a digital illustration class. I illustrated the peaceful but lonely feeling of looking up at the moon on a clear night."},
+    { name: "Afternoon in the Fairy Forest", source: FairyForest, medium: "Digital, Clip Studio Paint", desc: "Anatomy and perspective assignment. The prompt was to place 10 figures into an environment, while using correct anatomy and perspective" },
+    { name: "Warm Citrus", source: Citrus, medium: "Ink and Colored Pencil", desc: "Created for a materials and techniques class. Free range of subject matter" },
+    { name: "Mushroom Date", source: MushroomDate, medium: "Digital, Clip Studio Paint", desc: "Personal piece for a yearly trend in the art community called drawtober. The prompt was \"mushroom\"." },
+    { name: "Homesick", source: homesick, medium: "Digital, Clip Studio Paint", desc: "Personal piece I made for Valentines day illustrating the bittersweetness of being in a long distance relationship"}, 
+    { name: "Autumn State of Mind", source: GreenSweater, medium: "Digital, Clip Studio Paint", desc: "Personal piece for a yearly trend in the ary community called drawtober. The prompt was \"sweater\"."},
+    { name: "Forest Pattern", source: ForestPattern, medium: "Digital, Clip Studio Paint", desc: "Personal piece practicing repeating pattern design, taking inspiration from nature."},
+    { name: "Spider Serg", source: speederman, medium: "Digital, Krita", desc: "Personal piece/gift" },
+    { name: "Fantasy Character Lineup", source: FantasyCL, medium: 'Digital, Clip Studio Paint', desc: "Assignment for intro to illustration class. Prompt was to create a lineup of 5 original characters from the same timeline and environment." },
+    { name: "Fantasy Scene", source: FantasyScene, medium: 'Digital, Clip Studio Paint', desc: "Assignment for intro to illustration class. The prompt was to place the previous original 5 characters interacting in an environment." },
+    { name: "Isometric Treehouse", source: IsoTree, medium: 'Digital, Clip Studio Paint', desc: "Assignment for anatomy and perspective class. The prompt was to create any type of environment using isometric perspective." },
+    { name: "Queen, Illustrated portrait", source: Queen, medium: 'Digital, Clip Studio Paint', desc: "Assignment for illustration beyond the page class. The prompt was to create a conceptual illustrated portrait for Rolling stones magazine. I chose to do the band Queen and turn them into Roman bust statues." },
+    { name: "Breakfast Pattern", source: BreakfastPatt, medium: 'Digital, Clip Studio Paint', desc: "Repeating pattern inspired by breakfast foods"},
+    { name: "Day Dreamer", source: DeerGirl, medium: 'Digital, Krita', desc: "Personal project/gift" },
+    { name: "Angelic Statue", source: classicStatue, medium: 'Ink and Colored Pencil',  desc: "Created for a materials and techniques class. Free range of subject matter." },
+    { name: "T.N.T", source: TigerBomb, medium: 'Digital, Krita', desc: "Personal piece/gift"},
+    { name: "Dizzy Dreamer", source: spaceFace, medium: 'Gouache paint on oval wood board', desc: "Personal piece. Practicing working with gouache." },
+    { name: "Canterville Ghost hero pose and turnarounds", source: GhostPose, medium: 'Digital, Clip Studio Paint', desc: "Character design assignment for beyond the page class. I decided to design the ghost from Oscar Wilde's The Canterville Ghost."},
+    { name: "Canterville Ghost expression sheet", source: GhostExp, medium: 'Digital, Clip Studio Paint', desc: "Continuation of character design assignment. The prompt was to illustrate the original character emoting three different emotions"},
+    { name: "Mushroom Elf", source: hannahElf, medium: 'Digital, Krita', desc: "Personal piece/gift" },
+    { name: "The Last Unicorn book cover", source: LastUnicorn,   medium: 'Digital, Krita', desc: "Assignment for into to illustration class. The prompt was to create a conceptual book cover of a children's story or movie. I chose the movie: The Last Unicorn." },
+    { name: "Cloud Pattern", source: CloudPattern, medium: 'Digital, Clip Studio Paint', desc: "Repeating pattern design inspired by clouds."},
+    { name: "Bloom", source: Bloom, medium: 'Digital, Clip Studio Paint', desc: "Assignment for illustration beyond the page class. The prompt was to illustrate a visual metaphor. My concept was to illustrate a nurturing relationship where both figures literally take care of each other in order to grow." },
+    { name: "Witches Room", source: WitchesRoom, medium: 'Digital Clip Studio', desc: "Assignment for anatomy and perspective class. The prompt was to create a space that a witch character would live in." },
+    { name: "Untamed Tiger", source: CoolCat, medium: 'Digital, Clip Studio Paint', desc: "Personal piece where I focused on colors, character design, and blending modes." },
+
+];
--- anna-iwin-front/src/pages/portfolio.js
+++ anna-iwin-front/src/pages/portfolio.js
@@ -1,180 +1,90 @@
 import React from 'react';
-//import { makeStyles } from '@material-ui/core/styles';
 
+//Modal 
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
-// import { getAverageColor } from 'fast-average-color-node';
 
+//CSS
 import "./portfolio.css";
 
+//Floating Action Button
+//import Fab from '@material-ui/core/Fab';
 
+//icons
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai"
 
-
-
-
-//Pieces
-import classicStatue from '../art/classicStatue.jpg';
-import spaceFace from '../art/spaceFace.jpg';
-import speederman from '../art/speederman.jpeg';
-import hannahElf from '../art/hannahElf.jpeg';
-import GoodnightMoon from '../art/Goodnight_Moon.jpg';
-import Citrus from '../art/Cictrus.jpg';
-import FairyForest from '../art/Fairy_Forest.jpg';
-import MushroomDate from '../art/Mushroom_date.jpg';
-import GreenSweater from  '../art/Green_sweater.jpg';
-import FantasyCL from '../art/Fantasy_Character_lineup.jpg';
-import FantasyScene from '../art/Fantasy_scene.jpg';
-import IsoTree from '../art/Isometric_treehouse.jpg'; 
-import Queen from '../art/Queen.jpg'; 
-import WitchPatt from '../art/Witchy_pattern.jpg'; 
-import DeerGirl from '../art/Deer_Girl.jpeg'; 
-import TigerBomb from '../art/Tiger_Bomb.jpeg'; 
-import LastUnicorn from '../art/The_Last_Unicorn_cover.jpg'; 
-import Bloom from '../art/Bloom.jpg'; 
-import WitchesRoom from '../art/Witches_room.jpg'; 
-import CoolCat from '../art/Cool_Cat.jpg'; 
-import PetComm from '../art/Pet_commission.jpg';
-
-
-
-
+import { pieces } from '../data/artInfo.js';
 
 import Modal from '@material-ui/core/Modal';
 
 
-//const useStyles = makeStyles((theme) => ({
-    // container: { //container holding the image and description
-
-    //     width: "20vw",
-    //     height: "20vw",
-    //     padding: '10px',
-    //     border: '5px solid purple',
-    //     //margin: "auto",
-    //     display: 'inline-block', 
-    //     position: 'relative',
-
-    // },
-    // desc: { //hold information below the picture
-    //     padding: "5px",
-    //     textAlign: "center",
-    // },
-    // image: { //picture in gallery
-    //     width: '20vw',
-    //     height: '',
-    //     cursor: "pointer", 
-    //     //WIP
-    //     //position: 'relative',
-    //     position: 'relative',
-    //     margin: 'auto',
-    // },
-    // imageInside: { //picture in modal-paper
-    //     ["@media (orientation: landscape)"]: { //when the screen is in landscape
-    //         height: '60vh',
-    //     },
-    //     ["@media (orientation: portrait)"]: {  //when the screen is in portrait
-    //         width: '60vw',
-    //     },
-    // },
-    // modal: { //modal holding paper div
-    //     display: 'flex',
-    //     alignItems: 'center',
-    //     justifyContent: 'center',
-
-    // },
-    // paper: { //paper holding picture, name, and information
-    //     backgroundColor: theme.palette.background.paper,
-    //     border: '2px solid #000',
-    //     boxShadow: theme.shadows[5],
-    //     padding: theme.spacing(2, 4, 3),
-    //     ["@media (orientation: landscape)"]: { //when the screen is in landscape
-    //         height: '90vh',
-    //         width: '70vw',
-    //     },
-    //     ["@media (orientation: portrait)"]: {  //when the screen is in portrait
-    //         width: '70vw',
-    //     },
-    //     //height: 'auto',
-    //     //width: '50vw',
-    //     textAlign: 'center',
-
-    // },
-    // body: {
-    //     textAlign: 'center',
-    // },
-//}));
-
-
 function Portfolio() {
 
-    //Style
-    //const classes = useStyles();
-
     const [open, setOpen] = React.useState(false);
-    //const [num, setNum] = React.useState(5);
 
-    //Data on peices and information 
-    const pieces = [
-        { name: "Goodnight Moon", source: GoodnightMoon, color: null, desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Citrus", source: Citrus, color: null, desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Fairy Forest", source: FairyForest, color: null, desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Mushroom Date", source: MushroomDate, color: null, desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Green Sweater", source: GreenSweater, color: null, desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Spider Serg", source: speederman, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Fantasy Character Lineup", source: FantasyCL, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Fantasy Scene", source: FantasyScene, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Isometric Treehouse", source: IsoTree, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Queen", source: Queen, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Witchy Pattern", source: WitchPatt, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Deer Girl", source: DeerGirl, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Statue", source: classicStatue, color: null, desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Tiger Bomb", source: TigerBomb, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Dizzy Dreamer", source: spaceFace, color: null, desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Mushroom Girl", source: hannahElf, color: null, desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "The Last Unicorn", source: LastUnicorn, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Bloom", source: Bloom, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Witches Room", source: WitchesRoom, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Cool Cat", source: CoolCat, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-        { name: "Pet Commission", source: PetComm, color: null, medium: '', desc: "Duis a porttitor ligula, in consequat nisi. Integer eu massa ac lorem suscipit tincidunt sed id nisi. Proin ultricies enim quis turpis volutpat sodales. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed efficitur congue nulla, id luctus lorem fermentum sit amet. Proin egestas elit erat, convallis congue leo aliquam sit amet." },
-    ];
 
     //Current Peice that is selected
     const [piece, setPiece] = React.useState(0);
 
 
+    // opens the modal when a peice is selected
     const handleOpen = (key) => {
         setOpen(true);
         setPiece(key);
+
+        console.log("Opening Artwork ID: ", key);
     };
 
+    // handles closing the modal
     const handleClose = () => {
         setOpen(false);
     };
 
-    /*
-        useEffect(() => {
-            pieces.map((p, key) => {
-                getAverageColor(p.source).then(col => {
-                    p.color = col.rgb;
-                    console.log(col);
-                    console.log("hello" + p.color);
-                });
-            });
-        });
-      */  
+    // moves to the next peice in the modal 
+    const handleNext = () => {
+        if (piece === pieces.length - 1) {
+            setPiece(0);
+        }
+        else {
+            setPiece(prevP => prevP + 1);
+        }
+    }
+
+    // moves to the previous peice in the modal
+    const handlePrev = () => {
+        if (piece === 0) {
+            setPiece(pieces.length - 1);
+        }
+        else {
+            setPiece(prevP => prevP - 1)
+        }
+    }
+
+
     return (
         <body class='body'>
             <h1>
                 Portfolio Page
             </h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a nisl a neque sodales egestas nec eu erat. Nullam aliquet mi in porttitor cursus. Praesent nec pretium leo, in lacinia dui. Etiam pellentesque leo nec ante placerat porttitor. Nulla euismod gravida scelerisque. Mauris consequat, ipsum ut dignissim malesuada, odio justo euismod turpis, eu iaculis augue ex eu lorem. Nulla sed molestie risus. Nulla a urna eu felis ultricies convallis. Nullam porttitor, lectus sed ullamcorper mattis, arcu tellus tristique dui, ac dapibus massa risus at quam.
+                The pieces I chose to include in my portfolio include school assignments, personal pieces, and pieces I created for others.
             </p>
 
+            <p>
+                click on any image to enhance and read details.
+            </p>
+
+            <p>
+                Click the arrows to switch to the next or previous image.
+            </p>
+
+
+            {/* Mapping through the Pieces data and presenting them in the container  */}
             {pieces.map((p, key) =>
-                    <div class='container'>
+                <div class='container'>
                     <img src={p.source} alt={p.name} class='image' onClick={(e) => handleOpen(key)} />
-                    
-                    </div>
+
+                </div>
             )}
 
 
@@ -195,9 +105,19 @@ function Portfolio() {
             >
                 <Fade in={open}>
                     <div class='paper'>
-                        <img src={pieces[piece].source} backgroundColor={pieces[piece].color} alt={pieces[piece].name} class='imageInside' />
+                        <img src={pieces[piece].source} alt={pieces[piece].name} class='imageInside' />
                         <h2 id="transition-modal-title">{pieces[piece].name}</h2>
                         <p id="transition-modal-description">{pieces[piece].desc}</p>
+
+                        <button onClick={handlePrev}>
+                            <AiOutlineArrowLeft />
+                        </button>
+
+                        <button onClick={handleNext}>
+                            <AiOutlineArrowRight />
+                        </button>
+
+
                     </div>
                 </Fade>
             </Modal>
